6ed3ae64a8bf44a8343d753fd5c412b3b56aeac2
Igor Topal <mr.topal99@yandex.ru>
2021-08-27 16:02:31 +0300

    Изменил логику добавления карточек

--- script.js
+++ script.js
@@ -20,41 +20,77 @@ profileAddButton.addEventListener('click', function(){
   popupImage.classList.add('popup_opened');
 })
 
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+const nameInput = formElement.querySelector('.popup__field-to-fill_name');
+const jobInput = formElement.querySelector('.popup__field-to-fill_description');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    const profileNameText = document.querySelector('.profile__name-text');
+    const profileDescription = document.querySelector('.profile__description');
+    profileNameText.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    popupName.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cards = document.querySelector('.cards');
+
+initialCards.forEach(function(elem){
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = elem.link;
+  cardElement.querySelector('.card__title').textContent = elem.name;
+
+  cards.prepend(cardElement);
+});
 
 
+const placeFormElement = popupImage.querySelector('.popup__form');
 
 
+function cardSubmitHandler (evt) {
+  evt.preventDefault();
+  const cardElement = cardTemplate.cloneNode(true);
+  const placeFieldToFill = popupImage.querySelector('.popup__field-to-fill_name').value;
+  const srcFieldToFill = popupImage.querySelector('.popup__field-to-fill_src').value;
+  cardElement.querySelector('.card__image').src = srcFieldToFill;
+  cardElement.querySelector('.card__title').textContent = placeFieldToFill;
+  cards.prepend(cardElement);
+  popupImage.classList.remove('popup_opened');
+  popupImage.querySelector('.popup__field-to-fill_name').value = '';
+  popupImage.querySelector('.popup__field-to-fill_src').value = '';
+}
 
-
-
-
-
-
-
-
-
-
-
-// // Находим форму в DOM
-// const formElement = document.querySelector('.popup__form');
-// // Находим поля формы в DOM
-// const nameInput = formElement.querySelector('.popup__field-to-fill_name');
-// const jobInput = formElement.querySelector('.popup__field-to-fill_description');
-
-// // Обработчик «отправки» формы, хотя пока
-// // она никуда отправляться не будет
-// function formSubmitHandler (evt) {
-//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-//                                                 // Так мы можем определить свою логику отправки.
-//                                                 // О том, как это делать, расскажем позже.
-
-//     nameInput.value
-
-//     // Выберите элементы, куда должны быть вставлены значения полей
-
-//     // Вставьте новые значения с помощью textContent
-// }
-
-// // Прикрепляем обработчик к форме:
-// // он будет следить за событием “submit” - «отправка»
-// formElement.addEventListener('submit', formSubmitHandler);
+placeFormElement.addEventListener('submit', cardSubmitHandler);
